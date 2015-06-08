305b063a612ef68026644179e2170264110c4899
Alexey <grimzest@gmail.com>
2015-06-08 10:03:17 +0300

    Alpha

---
+++ test.py
@@ -0,0 +1,62 @@
+import sys
+import getopt
+import xml.etree
+import xml.etree.ElementTree as ET
+
+
+def main(argv):
+    inputfile = ''
+    query_to_find = ''
+    replace_value = ''
+    query_to_delete = ''
+    outputfile = ''
+    tree = ''
+    root = ''
+    tree_out = ''
+    encoding = 'UTF-8'
+    count = 0
+
+    i = q = r = d = o = False
+
+    help_answer = 'Usage: xmlcat.py -i <file> -q <XPath> -r <value> -d <XPath> -o <outputfile> -e <encoding>'
+
+    try:
+        opts, args = getopt.getopt(
+            argv, "hi:q:o:r:d:e:", ["input=", "query=", "help=", "output=", "replace=", "delete=", "encoding="]
+        )
+
+    except getopt.GetoptError:
+        print(help_answer)
+        exit()
+
+    for opt, arg in opts:
+
+        if opt in ("-i", "--input"):
+            i = True
+            inputfile = arg
+
+        if opt in ("-q", "--query"):
+            q = True
+            query_to_find = arg
+
+        if opt in ("-o", "--output"):
+            outputfile = arg
+            o = True
+
+    if i:
+        tree = ET.parse(inputfile)
+        root = tree.getroot()
+        for node in root.findall('./node[@id="21729962"]'):
+            tree_out = ET.tostring(node)
+            count += 1
+        print("Найдено", count, "узлов")
+        tree_out = ET.ElementTree(ET.fromstring(tree_out))
+        #print(tree_out)
+        tree_out.write('out.xml', encoding, True)    
+    else:
+        print(help_answer)
+        exit()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- xmlcat.py
+++ xmlcat.py
@@ -1,106 +1,121 @@
 import sys
+import time
 import getopt
 import xml.etree
 import xml.etree.ElementTree as ET
-from xml.dom import minidom
 
 
-def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
-    """
-    rough_string = ET.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="\t")
-
+class Profiler(object):
+    def __enter__(self):
+        self._startTime = time.time()
+         
+    def __exit__(self, type, value, traceback):
+        print("Время затраченное на выполнение: {:.3f} sec".format(time.time() - self._startTime))
 
 def main(argv):
-    inputfile = ''
-    query_to_find = ''
-    replace_value = ''
-    query_to_delete = ''
-    outputfile = ''
-    tree = ''
-    root = ''
-    tree_out = ET.Element('nodes')
-    encoding = 'UTF-8'
+    with Profiler() as p:
+        inputfile = ''
+        query_to_find = ''
+        replace_value = ''
+        query_to_delete = ''
+        outputfile = ''
+        tree = ''
+        root = ''
+        tree_out = ''
+        node_string = ''
+        encoding = 'UTF-8'
+        count = 0
 
-    i = q = r = d = o = False
+        i = q = r = d = o = False
 
-    help_answer = 'Usage: xmlcat.py -i <file> -q <XPath> -r <value> -d <XPath> -o <outputfile> -e <encoding>'
+        help_answer = 'Usage: xmlcat.py -i <file> -q <XPath> -r <value> -d <XPath> -o <outputfile> -e <encoding>'
 
-    try:
-        opts, args = getopt.getopt(
-            argv, "hi:q:o:r:d:e:", ["input=", "query=", "help=", "output=", "replace=", "delete=", "encoding="])
+        try:
+            opts, args = getopt.getopt(
+                argv, "hi:q:o:r:d:e:", ["input=", "query=", "help=", "output=", "replace=", "delete=", "encoding="])
 
-    except getopt.GetoptError:
-        print(help_answer)
-        exit()
-
-    for opt, arg in opts:
-
-        if opt in ("-i", "--input"):
-            i = True
-            inputfile = arg
-
-        if opt in ("-q", "--query"):
-            q = True
-            query_to_find = arg
-
-        if opt in ("-r", "--replace"):
-            r = True
-            replace_value = arg
-
-        if opt in ("-d", "--delete"):
-            query_to_delete = arg
-            d = True
-
-        if opt in ("-e", "--encoding"):
-            encoding = arg
-
-        if opt in ("-o", "--output"):
-            outputfile = arg
-            o = True
-
-        if opt in ("-h", "--help"):
+        except getopt.GetoptError:
             print(help_answer)
             exit()
 
-    if i:
-        tree = ET.parse(inputfile)
-        root = tree.getroot()
+        for opt, arg in opts:
 
-        if q:
-            for node in root.findall(query_to_find):
+            if opt in ("-i", "--input"):
+                i = True
+                inputfile = arg
+
+            if opt in ("-q", "--query"):
+                q = True
+                query_to_find = arg
+
+            if opt in ("-r", "--replace"):
+                r = True
+                replace_value = arg
+
+            if opt in ("-d", "--delete"):
+                query_to_delete = arg
+                d = True
+
+            if opt in ("-e", "--encoding"):
+                encoding = arg
+
+            if opt in ("-o", "--output"):
+                outputfile = arg
+                o = True
+
+            if opt in ("-h", "--help"):
+                print(help_answer)
+                exit()
+
+        if i:
+            tree = ET.parse(inputfile)
+            root = tree.getroot()
+
+            if q:
                 if r:
-                    node.text = replace_value
-                tree_out.extend(node)
+                    for node in root.findall(query_to_find):
+                        node.text = replace_value
+                        count += 1
+                    print("Изменено", count, "узлов.")
+                else:
+                    for node in root.findall(query_to_find):
+                        node_string = ET.tostring(node)
+                        tree_out += node_string
+                        count += 1
+                    print("Найдено", count, "узлов.")  
+                    tree_out = ET.ElementTree(ET.fromstring(tree_out))       
 
-        elif d:
-            if q:
-                root = tree_out.getroot()
-                for node in root.findall(query_to_delete):
-                    root.remove(node)
+            elif d:
+                if q:
+                    root = tree_out.getroot()
+                    for node in root.findall(query_to_delete):
+                        root.remove(node)
+                else:
+                    for node in root.findall(query_to_delete):
+                        root.remove(node)
+                        count += 1
+                    print("Удалено", count, "узлов.")    
+
             else:
-                for node in root.findall(query_to_delete):
-                    root.remove(node)
+                print(help_answer)
+                exit()
 
+            if o:
+                if q and not r:
+                    tree_out.write(outputfile, encoding, True)
+                else:
+                    tree.write(outputfile, encoding, True)
+            else:
+                outputfile = inputfile.split('.', 1)[0] + '_edited.xml'
+                #print(outputfile)
+                if q and not r:
+                    tree_out.write(outputfile, encoding, True)
+                else:
+                    tree.write(outputfile, encoding, True)
+            print("Отредактированный файл сохранен в ", outputfile,".")
         else:
             print(help_answer)
             exit()
-
-        if o:
-            if q:
-                ET(tree_out).write(outputfile, encoding, True)
-            else:
-                tree.write(outputfile, encoding, True)
-        else:
-            if q:
-                ET(tree_out).write(sys.stdout)
-            else:
-                tree.write(sys.stdout)
-    else:
-        print(help_answer)
-        exit()
 
 
 if __name__ == "__main__":
